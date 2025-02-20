1711bc17e72416dc3f70fabf88db69c13abb0a46
Vladimir Rudkovskiy <rudkovskyvladimir21051@gmail.com>
2025-02-20 10:12:13 +0200

    creating Header component with Dark mode toggle

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { ClerkProvider } from "@clerk/nextjs";
+import { ThemeProvider } from "@/components/ThemeProvider";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,14 +25,22 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-		<ClerkProvider>
-    <html lang="en">
+
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+				 <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
         {children}
+				</ThemeProvider>
       </body>
+			
     </html>
-		</ClerkProvider>
+
   );
 }
---
+++ components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import DarkModeToggle from './DarkModeToggle'
+
+
+function Header() {
+	return (
+		<div>
+			<DarkModeToggle />
+		</div>
+	)
+}
+
+export default Header
